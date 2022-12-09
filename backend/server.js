36f603063ce70cf7ba8cade621545272b65b95ce
marlon lee <marlon3@my.yorku.ca>
2022-12-09 12:26:34 -0500

    basic server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("Hi Marlon")
+const express = require('express')
+const dotenv = require('dotenv')
+
+const port = 5000
+
+const app = express()
+
+app.listen(port, () => console.log('server started on port 5000'))
