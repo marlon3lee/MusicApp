188b5ccc6944de86fc9fcefbaf1ce2ba93e7daa0
marlon lee <marlon3@my.yorku.ca>
2022-12-09 15:08:09 -0500

    errorHandler and asyncHandler

--- backend/controllers/artistsController.js
+++ backend/controllers/artistsController.js
@@ -1,30 +1,37 @@
+// use async package to handle try catches for promises when connecting to db so we can use our error handler
+const asyncHandler = require('express-async-handler')
+
 // @desc Get Artists
 // @route GET /Artists
 // @access Private (authentication)
-const getArtists = (req, res) => {
-    res.json({message: "Get Artists"})
-}
+const getArtists = asyncHandler(async (req, res) => {
+    res.status(200).json({message: "Get Artists"})
+})
 
 // @desc Create Artists
 // @route POST /Artists
 // @access Private (authentication)
-const createArtists = (req, res) => {
-    res.json({message: "Create Artists"})
-}
+const createArtists = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field');
+    }
+    res.status(200).json({message: "Create Artists"})
+})
 
 // @desc Update Artists
 // @route PUT /Artists
 // @access Private (authentication)
-const updateArtists = (req, res) => {
-    res.json({message: "Update Artists ${req.params.id}"})
-}
+const updateArtists = asyncHandler(async (req, res) => {
+    res.status(200).json({message: `Update Artists ${req.params.id}`})
+})
 
 // @desc Delete Artists
 // @route DELETE /Artists
 // @access Private (authentication)
-const deleteArtists = (req, res) => {
-    res.json({message: "Delete Artists ${req.params.id}"})
-}
+const deleteArtists = asyncHandler(async (req, res) => {
+    res.status(200).json({message: `Delete Artists ${req.params.id}`})
+})
 
 module.exports = {
     getArtists,
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware.js')
 
 const port = process.env.port || 5000
 
 const app = express()
 
+
+// middleware to recognize incoming request as json object or strings/arrays
+app.use(express.json())
+app.use(express.urlencoded({extended: false})) // parsing the URL-encoded data with querystring library (false)
+
 // loading artists router module into app
 app.use('/artists', require('./routes/artistsRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`server started on port ${port}`))
