46d62defd32900f4ddcabd4118a8f8dc8e329ff5
marlon lee <marlon3@my.yorku.ca>
2022-12-09 12:32:47 -0500

    environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 
-const port = 5000
+const port = process.env.port || 5000
 
 const app = express()
 
-app.listen(port, () => console.log('server started on port 5000'))
+app.listen(port, () => console.log(`server started on port ${port}`))
